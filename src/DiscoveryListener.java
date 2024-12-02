8d67816cb1d1a8c818e0c45f073491279239e41d
Sebastian Strozak <sstrozak01@qub.ac.uk>
2024-12-02 17:16:08 +0000

    discover

--- src/DiscoveryListener.java
+++ src/DiscoveryListener.java
@@ -16,26 +16,28 @@ public class DiscoveryListener extends Thread {
 
     public void run(){
         System.out.println("Starting discovery listening on port 4999");
-            try
-            {
-                MulticastSocket ms = new MulticastSocket(4999);
-                InetAddress ip = InetAddress.getByName("224.0.0.42");
-                ms.joinGroup(new InetSocketAddress(ip,4999),null);          
-                System.out.println(NetworkInterface.getNetworkInterfaces().toString());
+        String multicastGroupAddress = "230.0.0.0"; // Multicast group address
+        int port = 4999; // Port for the multicast
 
-                DatagramPacket packet;
-                while(true)
-                {
-                    byte[] byte_message = new byte[65535];
-                    packet = new DatagramPacket(byte_message, byte_message.length);
-                    ms.receive(packet);
-                    InetAddress remote_ip = packet.getAddress();
-                    node.receive(byte_message,remote_ip);
-                }
+        try (MulticastSocket socket = new MulticastSocket(port)) {
+            InetAddress group = InetAddress.getByName(multicastGroupAddress);
+
+            // Join the multicast group
+            socket.joinGroup(group);
+            System.out.println("Joined multicast group " + multicastGroupAddress);
+            DatagramPacket packet;
+            while (true){
+                byte[] byte_message = new byte[65535];
+                packet = new DatagramPacket(byte_message, byte_message.length);
+                socket.receive(packet);
+                InetAddress remote_ip = packet.getAddress();
+                node.receive(byte_message,remote_ip);
             }
-            catch(Exception e)
-            {
-                e.printStackTrace();
-            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
+
+        
